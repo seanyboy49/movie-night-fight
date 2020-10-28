09ae87f9eaeeef7f6bbb9562fc7db04442bd910a
sean lee <seanboramlee@gmail.com>
2020-10-27 19:12:42 -0700

    move CORS to api route

--- server/routes/api.py
+++ server/routes/api.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, jsonify
+from flask_cors import CORS
 
 api = Blueprint('api', __name__)
+# Enable CORS on api routes
+CORS(api)
+
+# Todo: create a route handler to authenticate requests
 
 @api.route('/api/hello')
-def hello():
-    print('hello world')
+def hello():    
     return jsonify('hello world')
