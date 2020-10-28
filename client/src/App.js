f4c972c81ddc6d2a93c65e60d99c232bfe30adac
sean lee <seanboramlee@gmail.com>
2020-10-27 20:59:10 -0700

    add POST to hello endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import logo from './logo.svg'
+import React, { useEffect, useState } from 'react'
+
 import './App.css'
 
 const API_URI =
@@ -8,6 +8,9 @@ const API_URI =
     : 'https://movienightfight.herokuapp.com/api'
 
 function App() {
+  const [formValue, setFormValue] = useState('')
+  const [displayValue, setDisplayValue] = useState('')
+
   useEffect(() => {
     async function getData() {
       try {
@@ -21,22 +24,38 @@ function App() {
 
     getData()
   }, [])
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    try {
+      const response = await fetch(`${API_URI}/hello`, {
+        method: 'POST',
+        body: JSON.stringify(formValue),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+
+      const data = await response.json()
+
+      setDisplayValue(data)
+    } catch (error) {
+      console.log('error', error)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+        />
+        <button onClick={handleSubmit}>submit</button>
+      </form>
+      <div>
+        <h1>{displayValue}</h1>
+      </div>
     </div>
   )
 }
--- server/routes/api.py
+++ server/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_cors import CORS
 
 api = Blueprint('api', __name__)
@@ -8,6 +8,10 @@ CORS(api)
 # Todo: create a route handler to authenticate requests
 
 
-@api.route('/api/hello')
+@api.route('/api/hello', methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+        data = request.get_json()
+        return jsonify(data)
+
     return jsonify('hello world')
