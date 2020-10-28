c585f7960a48574c35106ab017a63b90484a9248
sean lee <seanboramlee@gmail.com>
2020-10-27 18:35:12 -0700

    get routes.main to serve react app

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask 
+import os
 
 from .routes.main import main
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder=os.path.abspath('client/build'), static_url_path='/')
     
-    # register routes 
+    # register routes
     app.register_blueprint(main)
 
     return app
--- server/routes/main.py
+++ server/routes/main.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+import os
 
-main = Blueprint('main', __name__)
+main = Blueprint('main', __name__, static_folder=os.path.abspath("client/build"))
 
 @main.route('/')
 def index():
-    return 'hello'
+    return main.send_static_file('index.html')
