267b5750b4cc888be950b6240eed2419f123f6e2
sean lee <seanboramlee@gmail.com>
2020-10-28 23:45:11 -0700

    update to DATABASE_URL

--- server/settings.py
+++ server/settings.py
@@ -5,5 +5,5 @@ from pathlib import Path
 env_path = Path(".") / '.env'
 load_dotenv(dotenv_path=env_path)
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
