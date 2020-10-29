80fca5da64f7923a0b7dce20d3a0ac35fae62166
sean lee <seanboramlee@gmail.com>
2020-10-28 18:32:24 -0700

    install python-dotenv

--- server/settings.py
+++ server/settings.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(".") / '.env'
+load_dotenv(dotenv_path=env_path)
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
