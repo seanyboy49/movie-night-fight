bef9ca3488fc011c110ce3da45a139f1a2938a7e
sean lee <seanboramlee@gmail.com>
2020-11-18 21:10:47 -0800

    import User into commands

--- server/commands.py
+++ server/commands.py
@@ -2,11 +2,10 @@ import click
 from flask.cli import with_appcontext
 
 from .extensions import db
-from .models import Post
+from .models import Post, User
 
 
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
     db.create_all()
-
