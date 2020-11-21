5e619bb9c3765b01fc1808477c302fbdb413ba6b
sean lee <seanboramlee@gmail.com>
2020-11-20 21:34:22 -0800

    add basic form

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
-  return <h1>Login</h1>
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+
+  function onSubmit() {
+    console.log('submit')
+  }
+
+  console.log('userName', userName)
+  console.log('password', password)
+  return (
+    <>
+      <h2>Login</h2>
+      <form>
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button onClick={onSubmit}>Login Now</button>
+      </form>
+    </>
+  )
 }
 
 export default Login
