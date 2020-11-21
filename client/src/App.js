6bc63a9365740531df12b302753d6126190eb6f4
sean lee <seanboramlee@gmail.com>
2020-11-20 21:23:34 -0800

    add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MoviesList from './pages/MoviesList'
 
 const API_URI =
   process.env.NODE_ENV === 'development'
@@ -8,61 +11,22 @@ const API_URI =
     : 'https://movienightfight.herokuapp.com/api'
 
 function App() {
-  const [formValue, setFormValue] = useState([])
-  const [displayValue, setDisplayValue] = useState('')
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const response = await fetch(`${API_URI}/hello`)
-        const data = await response.json()
-
-        setDisplayValue(data)
-      } catch (error) {
-        console.log('error', error)
-      }
-    }
-
-    getData()
-  }, [])
-
-  async function handleSubmit(e) {
-    e.preventDefault()
-    try {
-      const response = await fetch(`${API_URI}/hello`, {
-        method: 'POST',
-        body: JSON.stringify(formValue),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-
-      const data = await response.json()
-
-      setDisplayValue(displayValue.concat(data))
-      setFormValue('')
-    } catch (error) {
-      console.log('error', error)
-    }
-  }
-
-  if (!displayValue) return null
-
   return (
-    <div className="App">
-      <form>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-        />
-        <button onClick={handleSubmit}>submit</button>
-      </form>
-      <div>
-        {displayValue.map((v) => (
-          <h4 key={v}>{v}</h4>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route path="/home">
+          <Home />
+        </Route>
+
+        <Route path="/movies-list">
+          <MoviesList />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
