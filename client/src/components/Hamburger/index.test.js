8f21f72f0f11f3ebd6a21546969573d878277198
sean lee <seanboramlee@gmail.com>
2020-11-25 11:38:26 -0800

    test for Burger

--- client/src/components/Hamburger/index.test.js
+++ client/src/components/Hamburger/index.test.js
@@ -1,10 +1,24 @@
 import { shallow } from 'enzyme'
 import Burger from '.'
 
+const onClick = jest.fn()
+const defaultProps = {
+  onClick,
+  isOpen: false,
+}
+
 describe('<Burger />', () => {
   it('should render', () => {
-    const wrapper = shallow(<Burger />)
+    const wrapper = shallow(<Burger {...defaultProps} />)
 
     expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should handle onClick', () => {
+    const wrapper = shallow(<Burger {...defaultProps} />)
+
+    wrapper.simulate('click')
+
+    expect(onClick).toHaveBeenCalled()
   })
 })
