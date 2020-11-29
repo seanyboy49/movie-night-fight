7c48eb9d480e215b3a00fa4a359758234af21c5b
Jina Zhu <jinazhu87@gmail.com>
2020-11-28 20:27:03 -0800

    redirect

--- client/src/components/AccountPrompt/index.js
+++ client/src/components/AccountPrompt/index.js
@@ -12,7 +12,7 @@ import {
   PromptContainer,
 } from './styled'
 import { H2 } from '../../styles/Text'
-import { login, logout, useAuth } from '../../auth'
+import { login, useAuth } from '../../auth'
 
 const AccountPrompt = ({ text, page, pageHeader, pageText, link }) => {
   const [isLoading, setIsLoading] = useState(false)
@@ -47,36 +47,36 @@ const AccountPrompt = ({ text, page, pageHeader, pageText, link }) => {
     }
   }
 
+  if (logged) {
+    history.push('/movies-list')
+  }
+
   return (
     <PromptContainer>
-      {logged ? (
-        <Button text={'logout'} onSubmit={logout} />
-      ) : (
-        <FormContainer>
-          <H2>{pageHeader}</H2>
-          <Form>
-            <Input
-              type="text"
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <Input
-              type="password"
-              placeholder="Password"
-              autoComplete="new-password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              text={text}
-              onSubmit={onSubmit}
-              isLoading={isLoading}
-              isDisabled={isFormInvalid}
-            />
+      <FormContainer>
+        <H2>{pageHeader}</H2>
+        <Form>
+          <Input
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            autoComplete="new-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            text={text}
+            onSubmit={onSubmit}
+            isLoading={isLoading}
+            isDisabled={isFormInvalid}
+          />
 
-            <PromptLink to={`/${link}`}>{pageText}</PromptLink>
-          </Form>
-        </FormContainer>
-      )}
+          <PromptLink to={`/${link}`}>{pageText}</PromptLink>
+        </Form>
+      </FormContainer>
     </PromptContainer>
   )
 }
@@ -86,6 +86,7 @@ Button.propTypes = {
   page: PropTypes.string.isRequired,
   pageHeader: PropTypes.string.isRequired,
   pageText: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
 }
 
 export default AccountPrompt
