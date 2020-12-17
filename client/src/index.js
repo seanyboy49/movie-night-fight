f939ba9a72cb36ce4073a77a509b42f731c78b3e
Jina Zhu <jinazhu87@gmail.com>
2020-12-16 17:09:18 -0800

    address PR comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { createStore } from 'redux'
 import allReducers from './reducers'
-import { Provider } from 'react-redux'
 
 const store = createStore(
   allReducers,
