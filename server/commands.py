c5a68d3d27b6b85235c7e29ac0fe4a12758fffcf
sean lee <seanboramlee@gmail.com>
2020-12-20 22:11:34 -0800

    add create_users_movies

--- server/commands.py
+++ server/commands.py
@@ -1,8 +1,8 @@
 import click
 from flask.cli import with_appcontext
 
-from .extensions import db, guard
-from .models import User
+from .extensions import db
+from .seed import create_users, create_movies
 
 
 @click.command(name='create_tables')
@@ -11,13 +11,8 @@ def create_tables():
     db.create_all()
 
 
-@click.command(name='create_first_user')
+@click.command(name='create_users_movies')
 @with_appcontext
-def create_first_user():
-    if db.session.query(User).filter_by(username='Sean').count() < 1:
-        db.session.add(User(
-            username='Sean',
-            password=guard.hash_password('strongpassword'),
-            roles='admin'
-        ))
-    db.session.commit()
+def create_users_movies():
+    create_users()
+    create_movies()
