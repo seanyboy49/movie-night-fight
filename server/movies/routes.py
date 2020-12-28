fbc1a7b099e95120ff95333544fe7520f3df7e17
sean lee <seanboramlee@gmail.com>
2020-12-27 18:47:41 -0800

    update auth routes

--- server/movies/routes.py
+++ server/movies/routes.py
@@ -3,20 +3,26 @@ from flask_praetorian import auth_required, current_user
 
 from server.movies import movies_bp
 
+
 @movies_bp.route('/api/movies')
 @auth_required
 def get_movies():
-    user = current_user()
+    try:
+        user = current_user()
 
-    response = []
-    unwatched = list(filter(lambda m: m.watched_at == None, user.watchlist))
+        response = []
+        unwatched = list(filter(lambda m: m.watched_at == None, user.watchlist))
 
-    for user_movie in unwatched:
-        movie = {
-            'name': user_movie.movie.name,
-            'omdb_id': user_movie.movie.omdb_id,
-            'poster_url': user_movie.movie.poster_url
-        }
-        response.append(movie)
+        for user_movie in unwatched:
+            movie = {
+                'name': user_movie.movie.name,
+                'omdb_id': user_movie.movie.omdb_id,
+                'poster_url': user_movie.movie.poster_url
+            }
+            response.append(movie)
 
-    return jsonify(response)
+        return jsonify(response)
+
+    except Exception as e:
+        print('error')
+        return e
