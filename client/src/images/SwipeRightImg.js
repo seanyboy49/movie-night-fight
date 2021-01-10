f959bf78fb0f20eb3302d74a169dea0f6ee202c3
sean lee <seanboramlee@gmail.com>
2021-01-09 21:06:03 -0800

    add animation to swipe images

--- client/src/images/SwipeRightImg.js
+++ client/src/images/SwipeRightImg.js
@@ -1,32 +1,47 @@
 import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+const rotationKeyFrames = keyframes`
+  from {
+    transform: rotateZ(0deg);
+  }
+  to {
+    transform:rotateZ(50deg);
+  }
+`
+const AnimationContainer = styled.div`
+  animation: ${rotationKeyFrames} infinite ease alternate-reverse 1s;
+`
 
 const SwipeRightImg = () => {
   return (
-    <svg
-      width="169"
-      height="228"
-      viewBox="0 0 169 228"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M57 228V211L33.5 188L28 167.5L16 146L21.5 134H33.5L51.5 163.5V77.5L61.5 71L72.5 77.5V120.5M72.5 134V120.5M72.5 120.5L82 113.5L91 120.5M91 120.5V134M91 120.5L103 113.5L111.5 120.5M111.5 120.5V134M111.5 120.5L123.5 113.5L132 120.5L131 172.5L116.5 211V228"
-        stroke="white"
-        strokeWidth="7"
-      />
-      <path
-        d="M9.33393 58.7871C41.86 11.2024 108.243 -4.07181 164.163 56.262"
-        stroke="white"
-        strokeWidth="8"
-        strokeLinecap="round"
-      />
-      <path
-        d="M165.631 22.4068L165.072 58.9025L129.03 61.3508"
-        stroke="white"
-        strokeWidth="6"
-        strokeLinecap="round"
-      />
-    </svg>
+    <AnimationContainer>
+      <svg
+        width="169"
+        height="228"
+        viewBox="0 0 169 228"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M57 228V211L33.5 188L28 167.5L16 146L21.5 134H33.5L51.5 163.5V77.5L61.5 71L72.5 77.5V120.5M72.5 134V120.5M72.5 120.5L82 113.5L91 120.5M91 120.5V134M91 120.5L103 113.5L111.5 120.5M111.5 120.5V134M111.5 120.5L123.5 113.5L132 120.5L131 172.5L116.5 211V228"
+          stroke="white"
+          strokeWidth="7"
+        />
+        <path
+          d="M9.33393 58.7871C41.86 11.2024 108.243 -4.07181 164.163 56.262"
+          stroke="white"
+          strokeWidth="8"
+          strokeLinecap="round"
+        />
+        <path
+          d="M165.631 22.4068L165.072 58.9025L129.03 61.3508"
+          stroke="white"
+          strokeWidth="6"
+          strokeLinecap="round"
+        />
+      </svg>
+    </AnimationContainer>
   )
 }
 
