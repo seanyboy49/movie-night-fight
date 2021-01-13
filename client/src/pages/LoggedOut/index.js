7823b095af544a72f92c17c6b9f3dfcbb4cae8a5
sean lee <seanboramlee@gmail.com>
2021-01-12 22:52:22 -0800

    redirect to app if logged in and visit public page

--- client/src/pages/LoggedOut/index.js
+++ client/src/pages/LoggedOut/index.js
@@ -6,10 +6,18 @@ import Login from '../Login'
 import Signup from '../SignUp'
 import NavigationHeader from '../../components/NavigationHeader'
 import routes from '../../routes'
+import { useAuth } from '../../auth'
 
 const { root, home, login, signup } = routes.public
+const { root: appRoot } = routes.app
 
 const LoggedOut = () => {
+  const [logged] = useAuth()
+
+  if (logged) {
+    return <Redirect to={appRoot} />
+  }
+
   return (
     <>
       <NavigationHeader background="#D70808" color={'white'} />
