074c1990725a4d9db7b2a48afb2bf2df4cb13fff
sean lee <seanboramlee@gmail.com>
2021-01-13 21:01:42 -0800

    use actioncreator in createHouse

--- client/src/pages/MoviesList/index.js
+++ client/src/pages/MoviesList/index.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 import { MovieListBackground } from '../../styles/Background'
 import Marquee from '../../components/Marquee'
 import Posters from './Posters'
 import SelectedMovie from './SelectedMovie'
+import { success, failure } from '../../state/actions'
 
 const MoviesList = () => {
   const [selectedMovie, setSelectedMovie] = useState()
-
+  const dispatch = useDispatch()
   return (
     <MovieListBackground>
+      <button onClick={() => dispatch(success('Everything is awesome!'))}>
+        success
+      </button>
+      <button onClick={() => dispatch(failure('Putain de merde!'))}>
+        failure
+      </button>
       <Marquee />
       {!selectedMovie && <Posters setSelectedMovie={setSelectedMovie} />}
       {selectedMovie && <SelectedMovie selectedMovie={selectedMovie} />}
