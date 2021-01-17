090762194026616287f51e28246f2ea337c591eb
Jina Zhu <jinazhu87@gmail.com>
2021-01-16 18:54:10 -0800

    address pr and add proptypes to noMovieOrHouse component

--- client/src/pages/MoviesList/NoMoviesOrHouse.js
+++ client/src/pages/MoviesList/NoMoviesOrHouse.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import { NoMovieContainer, NoMoviePoster } from './styled'
 import { BebasText } from '../../styles/Text'
 import confused from '../../images/confused.svg'
 import TicketLink from './TicketLink'
 
-const NoMoviesOrHouse = ({ text, redirectTo, Add }) => {
+const NoMoviesOrHouse = ({ text, redirectTo }) => {
   return (
     <NoMovieContainer>
       <NoMoviePoster>
@@ -17,6 +18,11 @@ const NoMoviesOrHouse = ({ text, redirectTo, Add }) => {
       <TicketLink text={`Add ${text[1]}`} toLink={redirectTo} />
     </NoMovieContainer>
   )
+}
+
+NoMoviesOrHouse.propTypes = {
+  text: PropTypes.array.isRequired,
+  redirectTo: PropTypes.string.isRequired,
 }
 
 export default NoMoviesOrHouse
