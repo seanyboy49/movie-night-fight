bfebdd266e6b6aa823353141a85011f0732cc800
Jina Zhu <jinazhu87@gmail.com>
2021-01-20 21:16:13 -0800

    address pr comments

--- server/misc/__init__.py
+++ server/misc/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 misc_bp = Blueprint('misc', __name__)
 
-# Enable CORS on houses api routes
+# Enable CORS on misc api routes
 CORS(misc_bp)
 
 from server.misc import routes
--- server/misc/routes.py
+++ server/misc/routes.py
@@ -2,7 +2,6 @@ from flask import jsonify
 
 from server.misc import misc_bp
 
-
 @misc_bp.route('/api/ping')
 def ping_cron():
     return jsonify('hello world')
